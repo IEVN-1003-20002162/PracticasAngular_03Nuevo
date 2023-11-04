e1225cb3a319f27784aa3af4049e8027e736785b
carlosarias <c.04102002@gmail.com>
2023-11-03 21:06:35 -0600

    CEAB .1.11.23

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MenuComponent } from './menu/menu/menu.component';
+import { MaterialModule } from './material/material.module';
+import { DatosAlumnoComponent } from './formularios/datos-alumno/datos-alumno.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    DatosAlumnoComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/formularios/datos-alumno/datos-alumno.component.ts
@@ -0,0 +1,32 @@
+import { Component,OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validator, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-datos-alumno',
+  templateUrl: './datos-alumno.component.html',
+  styleUrls: ['./datos-alumno.component.css']
+})
+
+export class DatosAlumnoComponent implements OnInit {
+  alumnos!:FormGroup;
+  
+  ngOnInit(): void {
+    this.alumnos=new FormGroup({
+      matricula:new FormControl('',[Validators.required,Validators.minLength(3)]),
+      nombre :new FormControl('',[Validators.required]),
+      apaterno:new FormControl('',[Validators.required]),
+      amaterno:new FormControl('',[Validators.required]),
+      correo:new FormControl('',[Validators.required])
+    })  
+  }
+
+  onSubmit(){
+      console.log(this.alumnos.controls['matricula'].value)
+      console.log(this.alumnos.controls['nombre'].value)
+      console.log(this.alumnos.controls['apaterno'].value)
+      console.log(this.alumnos.controls['amaterno'].value)
+      console.log(this.alumnos.controls['correo'].value)
+
+  }
+
+}
